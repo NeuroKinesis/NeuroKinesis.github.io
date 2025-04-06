600b9b6d00ce1d090bf3b0147cefdd3d18531eaf
SwanGon <58622995+SwanGon@users.noreply.github.com>
2025-04-06 21:50:54 +0200

    Copying next project (#2)
    * copying next project
    
    * fix github pages
    
    * fix config path

--- next.config.ts
+++ next.config.ts
@@ -13,8 +13,7 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
-
+  basePath: "/NeuroKinesis.github.io",
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
